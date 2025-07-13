48c777dfa4f66de63d748daab578ab5b5df0129a
Abdelaziz kazoum <abdukazoum@gmail.com>
2025-07-14 01:47:05 +0200

    set password to accept null

--- services/libs/common/src/config/config.module.ts
+++ services/libs/common/src/config/config.module.ts
@@ -13,7 +13,7 @@ import * as Joi from 'joi';
         DB_HOST: Joi.string().required(),
         DB_PORT: Joi.number().default(3306),
         DB_USERNAME: Joi.string().required(),
-        DB_PASSWORD: Joi.string().required(),
+        // DB_PASSWORD: Joi.string().required(),
         DB_DATABASE: Joi.string().required(),
         JWT_SECRET: Joi.string().required(),
         JWT_EXPIRATION: Joi.string().default('1h'),
