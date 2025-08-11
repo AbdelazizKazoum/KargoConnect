118e301670171bab6bdc705df4f66ccc398dfc23
Abdelaziz kazoum <abdukazoum@gmail.com>
2025-08-11 02:15:48 +0200

    return users with the signedUrls

--- services/apps/api-gateway/src/users/users.controller.ts
+++ services/apps/api-gateway/src/users/users.controller.ts
@@ -1,10 +1,14 @@
+import { MinioService } from '@app/shared';
 import { Controller, Get, Inject, Param, ParseIntPipe } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 
 @Controller('users')
 export class UsersController {
-  constructor(@Inject('USERS_SERVICE') private client: ClientProxy) {}
+  constructor(
+    @Inject('USERS_SERVICE') private client: ClientProxy,
+    private readonly minioService: MinioService,
+  ) {}
 
   @Get(':id/public-profile')
   async getUserPublicProfile(@Param('id', ParseIntPipe) id: number) {
@@ -21,6 +25,43 @@ export class UsersController {
       this.client.send({ cmd: 'getUsers' }, ''),
     );
 
-    return users;
+    // Call the method to generate signed URLs
+    const usersWithSignedUrls = await this.enrichUsersWithSignedUrls(users);
+
+    return usersWithSignedUrls;
+  }
+
+  async enrichUsersWithSignedUrls(users: any[]) {
+    // Map users and enrich image URLs
+    return Promise.all(
+      users.map(async (user) => {
+        // Signed URL for user.image
+        const imageUrl = user.image
+          ? await this.minioService.getSignedUrl(user.image)
+          : null;
+
+        // Signed URLs for each vehicle image
+        const vehicles = await Promise.all(
+          (user.vehicles || []).map(async (vehicle) => {
+            const signedImages = await Promise.all(
+              (vehicle.images || []).map((key) =>
+                this.minioService.getSignedUrl(key),
+              ),
+            );
+
+            return {
+              ...vehicle,
+              images: signedImages,
+            };
+          }),
+        );
+
+        return {
+          ...user,
+          image: imageUrl,
+          vehicles,
+        };
+      }),
+    );
   }
 }
